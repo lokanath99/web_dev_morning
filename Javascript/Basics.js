802162486619830f4b92b5dcfaa0a3145384d239
“lokanath99” <lokanath1912@gmail.com>
2023-11-16 09:06:30 +0530

    16/11/23

---
+++ Javascript/Basics.js
@@ -0,0 +1,28 @@
+// declaring a variable
+// var a = 10;
+// let b = 10;
+// const C = 5;
+// console.log(a)
+// var a = 11;
+// b = 21;
+// a = 58;
+// C = 2;
+// console.log(C)
+
+// Data types
+// var i = 0;
+// console.log(typeof(i));
+
+// var s = 'S';
+// console.log(typeof(s));
+
+// var a; // unassigned a -> kind of null value
+// console.log(a);
+
+// var b = null;// null value
+// console.log(b);
+
+// boolean -> True, False
+var bol = true;
+console.log(typeof(bol));
+
